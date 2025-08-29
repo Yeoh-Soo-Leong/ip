5b65232c73a951d7333878a1c322c7f221df6798
YSL <88653493+Yeoh-Soo-Leong@users.noreply.github.com>
2025-08-29 08:45:50 +0800

    feat: add echo function

--- src/main/java/Luke.java
+++ src/main/java/Luke.java
@@ -1,11 +1,37 @@
+import java.util.Scanner;
+
 public class Luke {
+    public static void echo() {
+        Scanner keyboard = new Scanner(System.in);
+        String input = keyboard.nextLine();
+
+        while (!input.equals("bye")) {
+            String response = "____________________________________________________________\n" +
+                    input + "\n" +
+                    "____________________________________________________________\n";
+            System.out.println(response);
+            input = keyboard.nextLine();
+        }
+
+        keyboard.close();
+    }
+
     public static void main(String[] args) {
-        String greeting = "____________________________________________________________\n" +
-                " Hello! I'm Luke\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
+        String greeting = """
+                ____________________________________________________________
+                 Hello! I'm Luke
+                 What can I do for you?
+                ____________________________________________________________
+                """;
         System.out.println(greeting);
+
+        echo();
+
+        String exitResponse = """
+                ____________________________________________________________
+                Bye Bye
+                ____________________________________________________________
+                """;
+        System.out.println(exitResponse);
     }
 }
