33c9a1507578727e144ffabc76064ffd8b7fec1b
YSL <88653493+Yeoh-Soo-Leong@users.noreply.github.com>
2025-09-15 18:16:13 +0800

    feat: Add support for deleting tasks from list

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -8,6 +8,14 @@ public class List {
         System.out.println("added: " + task.getTaskDescription());
     }
 
+    public void removeTask(int index) {
+        System.out.println("removed: " + tasks[index].getTaskDescription());
+        this.tasks[index] = null;
+        for (int j = index; j < this.noTasks; j++)
+            this.tasks[j] = this.tasks[j + 1];
+        this.noTasks -= 1;
+    }
+
     public void listTasks() {
         for (int i = 0; i < this.noTasks; i += 1) {
             System.out.print((i + 1) + ".");
